c63a1cc798993af9ff955cf5a1949cd19e9facf1
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-23 20:25:56 -0500

    Added the bare minimum for the home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flutter',
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              'Blog',
+              style: TextStyle(fontSize: 22, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0, left: 30.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
