0ee215e541ce45dcfdc1fc1ec7339dbd74378251
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-27 13:00:32 -0500

    Added a blog model

---
+++ lib/models/blog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Blog {
+  String imageUrl;
+  String authorName;
+  String description;
+  int timePosted;
+
+  Blog({
+    this.imageUrl,
+    this.authorName,
+    this.description,
+    this.timePosted,
+  });
+}
